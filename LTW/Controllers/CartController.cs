03d794dfaca67dbf10d1d2738eb45669eecc5651
Mitke <tpnguyennam1215@gmail.com>
2025-12-12 18:04:46 +0700

    Giỏ Hàng
    sửa giỏ hàng trong layout, thêm CartFuntion, CartItemFuntion thay Cart, CartItem như trong bài của cô

--- LTW/Controllers/CartController.cs
+++ LTW/Controllers/CartController.cs
@@ -1,16 +1,133 @@
-﻿using System;
+﻿using LTW.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LTW.Models;
 
 namespace LTW.Controllers
 {
     public class CartController : Controller
     {
         // GET: Cart
+        LinhKienDienTuEntities_ db = new LinhKienDienTuEntities_();
+
         public ActionResult Index()
         {
+            CartFuntion cart = Session["Cart"] as CartFuntion;
+            if (cart == null)
+            {
+                cart = new CartFuntion();
+                Session["Cart"] = cart;   // phải gán lại
+            }
+            return View(cart);
+        }
+        public ActionResult AddToCart(int id)
+        {
+            if (Session["User"] == null) //phải đăng nhập mới cho thêm vào giỏ hàng
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            CartFuntion cart = (CartFuntion)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new CartFuntion();
+            }
+            int result = cart.Them(id);
+            if (result == 1) //thêm thành công
+            {
+                Session["Cart"] = cart; //cập nhật lại giỏ hàng trong session
+
+            }
+            else
+            {
+                return RedirectToAction("Detail", "Home", new { id = id });
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult RemoveFromCart(int id)
+        {
+            if (Session["User"] == null) //phải đăng nhập mới cho thêm vào giỏ hàng
+            {
+                return RedirectToAction("Login", "User");
+            }
+            CartFuntion cart = (CartFuntion)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new CartFuntion();
+            }
+            int result = cart.Xoa(id);
+            if (result == 1) //thêm thành công
+            {
+                Session["Cart"] = cart; //cập nhật lại giỏ hàng trong session
+
+            }
+            return RedirectToAction("Index", "Cart");
+        }
+        public ActionResult UpdateSLCart(int id, int num)
+        {
+            int result = -1;
+            CartFuntion cart = (CartFuntion)Session["Cart"];
+            if (num == -1)
+            {
+                result = cart.Giam(id);
+            }
+            else
+            {
+                result = cart.Them(id);
+            }
+
+            if (result == 1) // thành công
+            {
+                Session["Cart"] = cart;
+
+            }
+
+            return RedirectToAction("Index", "Cart");
+        }
+        public ActionResult PaymentConfirm()
+        {
+            var khachHang = (Users)Session["User"];
+            CartFuntion cart = (CartFuntion)Session["Cart"];
+            if (cart == null || cart.list.Count == 0)
+            {
+                return RedirectToAction("Index", "Cart");
+            }
+
+            Orders hoaDon = new Orders
+            {
+                UserID = khachHang.UserID,
+                //MaNV = 1,
+                OrderDate = DateTime.Now,
+                TotalAmount = cart.TongThanhTien(),
+                //Status = 1,
+                ShipAddress = khachHang.DiaChi,
+                //DaThanhToan = false
+            };
+
+            db.Orders.Add(hoaDon);
+            db.SaveChanges();
+
+            int maHoaDonMoi = hoaDon.OrderID;
+
+            foreach (var item in cart.list)
+            {
+                OrderItems chiTiet = new OrderItems
+                {
+                    OrderID = maHoaDonMoi, // Gán MaHD đã lấy được
+                    ProductID = item.ProductID,
+                    Quantity = item.Quantity,
+                    Price = item.Price // Giá bán tại thời điểm đặt hàng
+                };
+                db.OrderItems.Add(chiTiet);
+            }
+
+            db.SaveChanges();
+
+            cart = new CartFuntion();
+            Session["Cart"] = cart; //cập nhật lại giỏ hàng trong session
             return View();
         }
     }
---
+++ LTW/Models/CartItemFuntion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LTW.Models
+{
+	public class CartItemFuntion
+	{
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ImageURL { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal ThanhTien
+        {
+            get { return Price * Quantity; }
+        }
+        LinhKienDienTuEntities_ db = new LinhKienDienTuEntities_();
+        public CartItemFuntion(int id)
+        {
+            Products sp = db.Products.FirstOrDefault(x => x.ProductID == id);
+            if (sp != null)
+            {
+                ProductID = sp.ProductID;
+                ProductName = sp.ProductName;
+                ImageURL = sp.ImageURL;
+                Price = sp.Price;
+                Quantity = 1;
+            }
+        }
+    }
+}
